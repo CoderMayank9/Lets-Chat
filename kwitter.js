920b292eab2b8908c205b9d8acae168a4a712b41
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-01-04 16:37:10 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,15 @@
+function addUser()
+{
+username = document.getElementById("username").value;
+if (username.length == 0){
+document.getElementById("username").placeholder = "Username required";
+
+
+}
+else{
+localStorage.setItem("username",username);
+window.location = "kwitter_room.html";
+
+}
+
+}
